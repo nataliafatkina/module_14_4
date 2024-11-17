e6b29b4a1e9002b63b7bff70b852d73e063971d8
nataliafatkina <fatkina.natali@gmail.com>
2024-11-17 17:03:51 +0300

    Add files via upload

---
+++ crud_functions.py
@@ -0,0 +1,29 @@
+import sqlite3
+
+connection = sqlite3.connect('Products.db')
+cursor = connection.cursor()
+
+
+def initiate_db():
+    cursor.execute('''
+    CREATE TABLE IF NOT EXISTS Products(
+    id INTEGER PRIMARY KEY,
+    title TEXT NOT NULL,
+    description TEXT,
+    price INTEGER NOT NULL
+    )
+    ''')
+
+    connection.commit()
+
+
+def get_all_products():
+    cursor.execute('SELECT * FROM Products')
+    return cursor.fetchall()
+
+
+initiate_db()
+
+# for i in range(1, 5):
+#     cursor.execute('INSERT INTO Products (title, description, price) VALUES (?, ?, ?)',
+#                    (f'Продукт {i}', f'Описание {i}', str(i * 100)))
